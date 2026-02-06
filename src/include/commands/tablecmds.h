4126882e51d0fa36c92d0fe3b8a76d741af357fa
Erik Wienhold <ewie@ewie.name>
2026-02-06 19:45:41 +0100

    fixup! [code-review] define constant for default tablespace

--- src/include/commands/tablecmds.h
+++ src/include/commands/tablecmds.h
@@ -21,6 +21,12 @@
 #include "storage/lockdefs.h"
 #include "utils/relcache.h"
 
+/*
+ * Tablespace name that instructs ALTER TABLE to assign the default
+ * tablespace.
+ */
+#define AT_TABLESPACE_DEFAULT ""
+
 typedef struct AlterTableUtilityContext AlterTableUtilityContext;	/* avoid including
 																	 * tcop/utility.h here */
 
